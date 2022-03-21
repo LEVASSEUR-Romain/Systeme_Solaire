8ebaa84162c23b9fded54d085a0ac3b07c4cf5dc
Romain <romainlev@gmail.com>
2022-03-21 23:31:44 +0100

    First

--- js/draw.js
+++ js/draw.js
@@ -1,3 +1,16 @@
+/* const grd = context.createRadialGradient(
+  0,
+  0,
+  celestialBody.radius,
+  0,
+  0,
+  celestialBody.radius * 3
+);
+grd.addColorStop(0.8, "black");
+grd.addColorStop(1, "white");
+context.fillStyle = grd;
+ */
+
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 function drawSquare() {
@@ -28,18 +41,35 @@ function drawSolarSystem() {
 }
 
 function drawCelestialBody(celestialBody) {
+  //Saves the context in its current state [*0]
   context.save();
 
   context.rotate(celestialBody.orbitalAngle);
+
+  //Translates the coordinate system with the vector (celestialBody.distance ; 0)
   context.translate(celestialBody.distance, 0);
 
   if (celestialBody.hasShadow) {
+    //Draws a black disque which will be the shadowed part of the planet
     context.beginPath();
+    const grd = context.createRadialGradient(
+      0,
+      0,
+      celestialBody.radius,
+      0,
+      0,
+      celestialBody.radius * 2
+    );
+    grd.addColorStop(0, "black");
+    grd.addColorStop(0.8, "white");
     context.arc(0, 0, celestialBody.radius, 0, 2 * Math.PI);
-    context.fillStyle = "#000000";
+    context.fillStyle = grd;
     context.fill();
+
+    //Saves the current context [*1]
     context.save();
 
+    //Prepares the drawing of the mask
     context.beginPath();
     context.arc(
       -celestialBody.radius * 2,
@@ -48,52 +78,52 @@ function drawCelestialBody(celestialBody) {
       0,
       2 * Math.PI
     );
-    // ICI JE GALERE
-    // Enregistrez le contexte actuel, préparez le dessin d’un cercle de la taille de la planète et activez le masque.
-    context.save();
-    //  Créez un dégradé radial transparent au centre et noir sur le pourtour. Les coordonnées du dégradé devront correspondre à celles du masque qui a permis de créer la face éclairée de la planète
-    const grd = context.createRadialGradient(
-      0,
-      0,
-      1,
-      celestialBody.radius,
-      0,
-      2 * Math.PI
-    );
-    grd.addColorStop(0, "white");
-    grd.addColorStop(1, "black");
-    context.fillStyle = grd;
-    context.fill();
-    context.restore();
 
+    //Create a mask from the previous prepared drawing
     context.clip();
   }
 
+  //Starts the drawing
   context.beginPath();
 
+  //Prepare the drawing of a complete circle
   context.arc(0, 0, celestialBody.radius, 0, 2 * Math.PI);
 
+  //Creates a pattern from the texture of the celestial body
   const pattern = context.createPattern(celestialBody.texture, "no-repeat");
   const coef = (celestialBody.radius * 2) / celestialBody.texture.width;
 
+  //Saves the current context [*2]
   context.save();
 
+  //Rotates the celestial body on its own axis
   context.rotate(celestialBody.rotationAngle);
 
+  //Moves and scales the coordinate system to apply the pattern
   context.translate(-celestialBody.radius, -celestialBody.radius);
   context.scale(coef, coef);
 
-  context.fillStyle = pattern;
+  //Sets the filling color
+  context.fillStyle = pattern; //celestialBody.color;
+
+  //Fills the circle
   context.fill();
+
+  //Restores the context [*2]
   context.restore();
 
   if (celestialBody.hasShadow) {
+    //Restores the context and disable the mask [*1]
     context.restore();
   }
+
+  //Draws each satellite of the celestial body
   celestialBody.satellites.forEach((satellite) => {
     drawOrbit(satellite);
     drawCelestialBody(satellite);
   });
+
+  //Restores the context on its initial state [*0]
   context.restore();
 }
 
---
+++ test/script.js
@@ -0,0 +1,22 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var x = 250,
+  y = 250,
+  // Radii of the white glow.
+  innerRadius = 32,
+  outerRadius = 70,
+  // Radius of the entire circle.
+  radius = 60;
+
+var gradient = ctx.createRadialGradient(x, y, innerRadius, x, y, outerRadius);
+gradient.addColorStop(0.8, "white");
+gradient.addColorStop(0.9, "black");
+
+ctx.arc(x, y, radius, 0, 2 * Math.PI);
+
+ctx.fillStyle = gradient;
+ctx.fill();
